488497846660f46bc7f32050b7f7727f3fba4cd8
DAkbariSE <danial_akbari33@hotmail.com>
2025-08-10 22:06:32 +0200

    Implementing clean architecture for ordering api

--- src/Services/Ordering/Ordering.API/Program.cs
+++ src/Services/Ordering/Ordering.API/Program.cs
@@ -1,7 +1,16 @@
+using Ordering.API;
+using Ordering.Application;
+using Ordering.Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
 //Add Services
+builder.Services
+    .AddApplicationServices()
+    .AddInfrastructureServices(builder.Configuration)
+    .AddApiServices();
 
 //Add Pipelines
 var app = builder.Build();
+app.AddApiApps();
 
 app.Run();
